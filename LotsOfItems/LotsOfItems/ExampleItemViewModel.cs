558609e277337f1516c6c6e43766dca49d7db2c3
ITSPROJECT\lalf <lalf@cin.ufpe.br>
2017-03-07 12:05:32 -0300

    Show in GirdView a lot of items using x:Bind with x:Phase and opacity technic

---
+++ LotsOfItems/LotsOfItems/ExampleItemViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.ObjectModel;
+
+namespace LotsOfItems
+{
+    public class ExampleItem
+    {
+        public string Title { get; set; }
+        public string Subtitle { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
+    }
+
+    public class ExampleItemViewModel
+    {
+        private ObservableCollection<ExampleItem> exampleItems = new ObservableCollection<ExampleItem>();
+        public ObservableCollection<ExampleItem> ExampleItems { get { return this.exampleItems; } }
+
+        public ExampleItemViewModel()
+        {
+            for (int i = 1; i < 150000; i++)
+            {
+                this.exampleItems.Add(new ExampleItem()
+                {
+                    Title = "Title: " + i,
+                    Subtitle = "Sub: " + i,
+                    Description = "Desc: " + i,
+                    ImagePath = "Assets/teste.jpg"
+                });
+            }
+        }
+    }
+}
---
+++ LotsOfItems/LotsOfItems/MainPage.xaml.cs
@@ -0,0 +1,63 @@
+﻿using Windows.UI.Xaml.Controls;
+
+namespace LotsOfItems
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+            this.ViewModel = new ExampleItemViewModel();
+        }
+
+        public ExampleItemViewModel ViewModel { get; set; }
+
+        // Display each item incrementally to improve performance.
+        private void GridView_ContainerContentChanging(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.Phase != 0)
+            {
+                throw new System.Exception("We should be in phase 0, but we are not.");
+            }
+
+            // It's phase 0, so this item's title will already be bound and displayed.
+
+            args.RegisterUpdateCallback(this.ShowSubtitle);
+
+            args.Handled = true;
+        }
+
+        private void ShowSubtitle(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.Phase != 1)
+            {
+                throw new System.Exception("We should be in phase 1, but we are not.");
+            }
+
+            // It's phase 1, so show this item's subtitle.
+            var templateRoot = args.ItemContainer.ContentTemplateRoot as StackPanel;
+            var textBlock = templateRoot.Children[1] as TextBlock;
+            textBlock.Text = (args.Item as ExampleItem).Subtitle;
+            textBlock.Opacity = 1;
+
+            args.RegisterUpdateCallback(this.ShowDescription);
+        }
+
+        private void ShowDescription(ListViewBase sender, ContainerContentChangingEventArgs args)
+        {
+            if (args.Phase != 2)
+            {
+                throw new System.Exception("We should be in phase 2, but we are not.");
+            }
+
+            // It's phase 2, so show this item's description.
+            var templateRoot = args.ItemContainer.ContentTemplateRoot as StackPanel;
+            var textBlock = templateRoot.Children[2] as TextBlock;
+            textBlock.Text = (args.Item as ExampleItem).Description;
+            textBlock.Opacity = 1;
+        }
+    }
+}
